9cad1cc2dbb4a0ec7e491150c06016f99fed3b20
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-07-01 08:05:56 +0600

    styling sidebar

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,11 +1,22 @@
 import "./Sidebar.css";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            Sidebar
-        </div>
-    );
+  return (
+    <section className="sidebar">
+      <button type="button">
+        <img src="src/assets/logo.png" alt="orvexa logo" />
+        <i className="fa-regular fa-pen-to-square"></i>
+      </button>
+      <ul className="history">
+        <li>Weekend Plan for Balance</li>
+        <li>Online Food Delivery Impact</li>
+        <li>Humanize AI Text</li>
+      </ul>
+      <div>
+        <p>Made with &hearts; by Orvexa</p>
+      </div>
+    </section>
+  );
 }
 
 export default Sidebar;
